baec6805d01b5821921ba69dc4c97e16cddceaab
banyan <ameutau@gmail.com>
2009-11-16 23:39:05 +0900

    パスを移動

--- initialize.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-# ~/git/config/ 以下で管理している dotfiles を $HOME にシンボリックリンクを貼る。
-# なお、指定したリンクファイルがすでにあった場合は、上書きするかどうか問い合わせを行い、
-# 上書きする場合には、.orig という suffix でファイルのバックアップを作成する。
-find ~/git/config -maxdepth 1 -name '.*' -exec ln -sbi --suffix=.orig {} ~/ \;
-
